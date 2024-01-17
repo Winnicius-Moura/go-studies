8a04a42316956d865c64282fadd61620efebd486
winnicius-moura <winnicius.moura@gmail.com>
2024-01-17 17:01:07 -0300

    feat: init routes

--- router/router.go
+++ router/router.go
@@ -4,10 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func Initialize() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	initializeRoutes(r)
+
+	r.Run(":8080")
 }
