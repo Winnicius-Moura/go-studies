a77283fd4907b4093f8e1af867600ab0eb9ecc6a
winnicius-moura <winnicius.moura@gmail.com>
2024-01-18 16:11:23 -0300

    feat: add custom Logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/Winnicius-Moura/go-studies.git/router"
+import (
+	"github.com/Winnicius-Moura/go-studies.git/config"
+	"github.com/Winnicius-Moura/go-studies.git/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
