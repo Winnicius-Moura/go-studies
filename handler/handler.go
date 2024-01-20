9f5de9ef164198a5c712851e9aaec4c555e70967
winnicius-moura <winnicius.moura@gmail.com>
2024-01-20 16:22:11 -0300

    feat: create createOpening handlerLogic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/Winnicius-Moura/go-studies.git/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db *gorm.DB
+)
+
+func InitializeHandler(){
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
